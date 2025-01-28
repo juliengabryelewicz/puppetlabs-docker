bfb4fbcd54e641c30e006cbcabd3ca8a8ae4aeef
Julien Gabryelewicz <julien.gabryelewicz2@gmail.com>
2025-01-28 18:17:43 +0100

    remove systemctl daemon-reload

--- manifests/systemd_reload.pp
+++ manifests/systemd_reload.pp
@@ -1,10 +1,3 @@
 # @summary
 #   For systems that have systemd
 #
-class docker::systemd_reload {
-  exec { 'docker-systemd-reload':
-    path        => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/',],
-    command     => 'systemctl daemon-reload',
-    refreshonly => true,
-  }
-}
